1ddf2d93a541949eb45d79d4519d4f5293e6cdb4
Vishwa-2057 <vishwa27032004@gmail.com>
2025-09-16 08:49:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is working!");
+});
+
+
 // Import routes
 const patientRoutes = require('./routes/patients');
 const appointmentRoutes = require('./routes/appointments');
